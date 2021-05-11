1a6cc5d14873bcbf29a4c692c31634ce962386e9
mcoyne16 <77979084+mcoyne16@users.noreply.github.com>
2021-05-11 11:28:24 -0300

    discover.py tests with params

--- web_app.py
+++ web_app.py
@@ -9,35 +9,43 @@ from dotenv import load_dotenv
 import requests
 from pandas import read_csv
 
-#api_key = os.environ.get("api_key")
-api_key = "c4c3cb40b87c5d67f381e5bbdc3763ca"
-print(api_key)
-request_url = f"https://api.themoviedb.org/3/movie/76341?api_key={api_key}"
-response = requests.get(request_url)
-print(type(response)) # class 'requests.models.response'
-print(response.status_code)
-print(response.text)
-#    if "Error Message" in response.text:
-#        print(f"Error, could not locate url.  Please try again.")
-#        continue
-#    if "higher API call frequency" in response.text:
-#        print("You entered too many stocks.  Please try again.")
-#        quit()
-#    parsed_response = json.loads(response.text)
-#    last_refreshed = parsed_response['Meta Data']['3. Last Refreshed']
-#    tsd = parsed_response['Time Series (Daily)']
-#    dates = list(tsd.keys()) #TODO sort
-#    latest_day = dates[0]
-#    latest_close = tsd[latest_day]['4. close']
+
+import tmdbsimple as tmdb
+tmdb.API_KEY='c4c3cb40b87c5d67f381e5bbdc3763ca'
+#search = tmdb.Search()
+#Userinput=input('Enter first movie name:')
+#response = search.movie(query=Userinput)
+#Userinput2=input('Enter Second movie name:')
+#response = search.movie(query=Userinput2)
+#for s in (search.results):
+# print(s['title'], s['popularity'])
 #
-#    high_prices = []
-#    low_prices = []
+#cal_start_date_str = '2014-12-11'
+#cal_end_date_str = '2014-12-12'
+discover = tmdb.Discover()
+results = discover.movie(
+    with_keywords = input("please select")
+#    release_date_gte=cal_start_date_str,
+#    release_date_lte=cal_end_date_str,
+#    sort_by="release_date.asc",
+)
 #
-#    for date in dates:
-#        high_price = tsd[date]["2. high"]
-#        high_prices.append(float(high_price))
-#        low_price = tsd[date]["3. low"]
-#        low_prices.append(float(low_price))
-#
-#    recent_high = max(high_prices) 
-#    recent_low = min(low_prices)
+for movie in results["results"]:
+    print("=" * 40)
+    print(movie["title"])
+    print(movie["id"])
+    print(movie["release_date"])
+    # Get the release dates for the movie
+    response = tmdb.movies.Movies(movie["id"]).release_dates()
+    results = response["results"][0]
+    release_dates = results["release_dates"]
+
+    for release in release_dates:
+        print("-" * 40)
+        print(f"release: {release}")
+        release_date_string = release["release_date"]
+        print(f"release_date_string: {release_date_string}")
+        # Check if this release date is in the range specified
+        release_datetime_begin = datetime.datetime.strptime(release["release_date"], "%Y-%m-%dT00:00:00.000Z")
+        release_date_begin = release_datetime_begin.date()
+        print(f"release_date_begin: {release_date_begin}")
