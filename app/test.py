6cb7ff400017e5192015fc43068dd5de83fa44e2
NikoRestifo <ndr45@georgetown.edu>
2021-05-15 13:08:02 -0400

    Added more input capabilities plus adjusted how parameters are taken if a field is left blank

--- app/test.py
+++ app/test.py
@@ -17,17 +17,53 @@ tmdb.API_KEY='c4c3cb40b87c5d67f381e5bbdc3763ca'
 def set_movie_settings():
     if APP_ENV == "development":
         movie_genre = str(input("Select Movie Genre: "))
-        #movie_year = input("Movies Released in Year")
+        vote_average = str(input("Minimum Movie Rating: "))
+        movie_year = str(input("Movies Released in Year: "))
+        runtime_min = str(input("Minimum Runtime: "))
+        runtime_max = str(input("Maximum Runtime: "))
+        movie_certification = str(input("Enter Movie Certification"))
         #movie_year_start = input("Movies Released After Year:")
     #else:
     #    user_country = COUNTRY_CODE
     #    user_zip = ZIP_CODE
-    return movie_genre #movie_year, #movie_year_start
+    return movie_genre, vote_average, movie_year, runtime_min, runtime_max, movie_certification #movie_year_start
 
-pick_movie_genre = str(set_movie_settings())
+pick_movie_genre, pick_vote_average, pick_movie_year, pick_runtime_min, pick_runtime_max, pick_certification = set_movie_settings()
 print(pick_movie_genre)
 print(type(pick_movie_genre))
 
+if bool(pick_movie_year) == True:
+    movie_year = int(pick_movie_year)
+else:
+    movie_year = None
+
+if bool(pick_vote_average) == True:
+    vote_average = float(pick_vote_average)
+else:
+    vote_average = None
+
+if bool(pick_runtime_min) == True:
+    runtime_min = float(pick_runtime_min)
+else:
+    runtime_min = None
+
+if bool(pick_runtime_max) == True:
+    runtime_max = int(pick_runtime_max)
+else:
+    runtime_max = None
+
+if bool(pick_runtime_max) == True:
+    runtime_max = int(pick_runtime_max)
+else:
+    runtime_max = None
+
+if bool(pick_certification) == True:
+    movie_certification= str(pick_certification)
+else:
+    movie_certification = None
+
+print(type(movie_year))
+print(type(vote_average))
 
 def genre_string_to_id():
     ids_list = []
@@ -55,9 +91,9 @@ while p<100:
 discover = tmdb.Discover()
 movie_ids = []
 for page_number in page_numbers:
-    response = discover.movie(sort_by="popularity.desc", with_genres=genre_number, vote_average_gte=6.8, 
-    primary_release_year=None, primary_release_date_gte=1980, with_runtime_gte=60, with_runtime_lte=180, 
-    certification_country="US", certification=None, page=page_number)
+    response = discover.movie(sort_by="popularity.desc", with_genres=genre_number, vote_average_gte=vote_average, 
+    primary_release_year=movie_year, primary_release_date_gte=None, with_runtime_gte=runtime_min, with_runtime_lte=runtime_max, 
+    certification_country="US", certification=movie_certification, page=page_number)
     for value in response['results']:
         id = value['id']
         movie_ids.append(id)
