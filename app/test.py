90c245dc4355e71d1bd16374561ae8843f802b49
NikoRestifo <ndr45@georgetown.edu>
2021-05-15 18:27:40 -0400

    Changed one input (exact movie year --> earliest movie year) and fixed corresponding code to ensure proper output

--- app/test.py
+++ app/test.py
@@ -18,24 +18,26 @@ def set_movie_settings():
     if APP_ENV == "development":
         movie_genre = str(input("Select Movie Genre: "))
         vote_average = str(input("Minimum Movie Rating: "))
-        movie_year = str(input("Movies Released in Year: "))
+        #movie_year_min = str(input("Movies Released in Year: "))
+        movie_year_min = str(input("Movies Release After Year: "))
         runtime_min = str(input("Minimum Runtime: "))
         runtime_max = str(input("Maximum Runtime: "))
-        movie_certification = str(input("Enter Movie Certification"))
+        movie_certification = str(input("Enter Movie Certification: "))
         #movie_year_start = input("Movies Released After Year:")
     #else:
     #    user_country = COUNTRY_CODE
     #    user_zip = ZIP_CODE
-    return movie_genre, vote_average, movie_year, runtime_min, runtime_max, movie_certification #movie_year_start
+    return movie_genre, vote_average, movie_year_min, runtime_min, runtime_max, movie_certification
 
-pick_movie_genre, pick_vote_average, pick_movie_year, pick_runtime_min, pick_runtime_max, pick_certification = set_movie_settings()
-print(pick_movie_genre)
-print(type(pick_movie_genre))
+pick_movie_genre, pick_vote_average, pick_movie_year_min, pick_runtime_min, pick_runtime_max, pick_certification = set_movie_settings()
 
-if bool(pick_movie_year) == True:
-    movie_year = int(pick_movie_year)
+if bool(pick_movie_year_min) == True:
+    movie_year_min = str(pick_movie_year_min)
 else:
-    movie_year = None
+    movie_year_min = None
+
+print(type(movie_year_min))
+print(movie_year_min)
 
 if bool(pick_vote_average) == True:
     vote_average = float(pick_vote_average)
@@ -43,7 +45,7 @@ else:
     vote_average = None
 
 if bool(pick_runtime_min) == True:
-    runtime_min = float(pick_runtime_min)
+    runtime_min = int(pick_runtime_min)
 else:
     runtime_min = None
 
@@ -62,8 +64,6 @@ if bool(pick_certification) == True:
 else:
     movie_certification = None
 
-print(type(movie_year))
-print(type(vote_average))
 
 def genre_string_to_id():
     ids_list = []
@@ -78,8 +78,6 @@ def genre_string_to_id():
     return genre_id
 
 genre_number=genre_string_to_id()
-print(type(genre_number))
-print(genre_number)
 
 page_numbers = []
 
@@ -92,7 +90,7 @@ discover = tmdb.Discover()
 movie_ids = []
 for page_number in page_numbers:
     response = discover.movie(sort_by="popularity.desc", with_genres=genre_number, vote_average_gte=vote_average, 
-    primary_release_year=movie_year, primary_release_date_gte=None, with_runtime_gte=runtime_min, with_runtime_lte=runtime_max, 
+    primary_release_year=None, primary_release_date_gte=(movie_year_min + "-01-01"), with_runtime_gte=runtime_min, with_runtime_lte=runtime_max, 
     certification_country="US", certification=movie_certification, page=page_number)
     for value in response['results']:
         id = value['id']
